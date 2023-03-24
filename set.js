a0d69b7d743d67861f2591321b1b49f38186f766
Vanderlei Kleinschmidt <vanderleik@yahoo.com.br>
2023-03-24 17:20:26 -0300

    Initial commit

---
+++ set.js
@@ -0,0 +1,17 @@
+//TDD Test Driven Develop
+var novoSet = new Set();
+
+class HistoryMap {
+
+}
+
+//11/11/2011 as 11 da noite
+novoSet.add('backend');
+novoSet.add('frontend');
+novoSet.add('mobile');
+novoSet.add('mobile');
+novoSet.add('mobile');
+
+
+var resultado = novoSet;
+console.log(resultado);
